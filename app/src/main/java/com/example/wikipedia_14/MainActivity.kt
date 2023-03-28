c7f396dc60cd5fe0eb6571f2f83a972e64f357d4
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-28 12:36:27 +0430

    implementation Navigation Drawer

--- app/src/main/java/com/example/wikipedia_14/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia_14/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.wikipedia_14
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import com.example.wikipedia_14.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -11,5 +17,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarMain)
+
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayoutMain,
+            binding.toolbarMain,
+            R.string.openDrawer,
+            R.string.closeDrawer
+        )
+        binding.drawerLayoutMain.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+
+        binding.navigationViewMain.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.menu_writer -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+
+
+                }
+
+                R.id.menu_photograph -> {
+
+
+                }
+
+                R.id.menu_vieo_maker -> {
+
+                }
+
+                R.id.menu_translator -> {
+
+
+                }
+
+                // ---------------------------------
+
+                R.id.menu_open_wikipedia -> {
+
+
+
+                }
+
+                R.id.openWikimedia -> {
+
+                }
+
+            }
+
+            true
+        }
+
     }
 }
